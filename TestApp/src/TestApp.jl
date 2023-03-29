0780ea139c1c0a6dc1d22a4331f72b2538ece2de
Will <carboxylmer@gmail.com>
2023-03-29 16:52:41 -0500

    more formatting and concise styles

--- TestApp/src/TestApp.jl
+++ TestApp/src/TestApp.jl
@@ -9,6 +9,8 @@ module TestApp
         change this, view the start method below.
     """
     function home(c::Connection)
+        mainstyle = ("border-style" => "solid", "border-width" => 5px, "border-radius" => 5px, "padding" => 5px, "margin" => 10px, "border-color" => "white")
+
         bod = body("mybody")
         
         maindiv = div("maindiv")
@@ -26,15 +28,15 @@ module TestApp
         # :text and :children
         drp_div = div("drp_div")
         push!(maindiv, drp_div)
-        style!(drp_div, "border-style" => "solid", "border-width" => 5px, "border-radius" => 5px, "padding" => 5px, "margin" => 10px, "border-color" => "white")
+        style!(drp_div, mainstyle...)
         opt1 = ToolipsDefaults.option("1", text="Fasfsadf")  # should display values? maybe?
         opt2 = ToolipsDefaults.option("2")
         opt2[:text] = "you can also set it this way!"
         drp = ToolipsDefaults.dropdown("drop", [opt1, opt2])
-        style!(drp, "border-style" => "solid", "border-width" => 5px, "border-radius" => 5px, "padding" => 5px, "margin" => 10px, "border-color" => "white")
+        style!(drp, mainstyle...)
         push!(drp_div, drp)
 
-        style!(bod, "background-color" => "#222222", "border-style" => "solid", "border-width" => 5px, "border-radius" => 5px, "padding" => 5px, "margin" => 10px, "border-color" => "white")
+        style!(bod, mainstyle..., "background-color" => "#555555", )
         
         
         write!(c, bod)
